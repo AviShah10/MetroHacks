3269174f947e26a3aeeb7727a8077c214b27e3e3
DanukGOAT <44783244+DanukGOAT@users.noreply.github.com>
2019-05-25 18:20:39 -0400

    Ayyy lets goooo

--- test.py
+++ test.py
@@ -3,37 +3,35 @@ import csv
 import tweepy
 import re
 
+print('hello')
 
-def search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase):
+if __name__ == '__main__':
+    # def search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase):
     # create authentication for accessing Twitter
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    ACCESS_TOKEN = '1132298551411630080-TxbsTVojtHBCpZC0pjgCbDYsx3XcwO'
+    ACCESS_SECRET = 'MjksGvOspEXk7EF8HUVgmgHJpTIgAifI5SY8SWpLyqeaI'
+    CONSUMER_KEY = '9Q7tOjRTcnk0GDgLGfMYESmFx'
+    CONSUMER_SECRET = 'Oy98g2cuXlPkTs75akfeVvDq2YiPiMvzSP9e4Z2wOronPBaGDp'
+    hashtag_phrase = input('Hashtag Phrase ')
 
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     # initialize Tweepy API
     api = tweepy.API(auth)
 
     # get the name of the spreadsheet we will write to
-    fname = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
+    # fname = '_'.join(re.findall(r'#(\w+)', hashtag_phrase))
 
     # open the spreadsheet we will write to
-    with open('%s.csv' % (fname), 'wb') as file:
-        w = csv.writer(file)
+    with open("alabama.csv", 'wt', encoding="utf8") as output:
+        w = csv.writer(output)
 
         # write header row to spreadsheet
         w.writerow(['timestamp', 'tweet_text', 'username', 'all_hashtags', 'followers_count'])
 
         # for each tweet matching our hashtags, write relevant info to the spreadsheet
-        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase + ' -filter:retweets', \
-                                   lang="en", tweet_mode='extended').items(100):
+        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase + ' -filter:retweets',
+                                   lang="en", tweet_mode='extended').items(10):
             w.writerow([tweet.created_at, tweet.full_text.replace('\n', ' ').encode('utf-8'),
                         tweet.user.screen_name.encode('utf-8'),
                         [e['text'] for e in tweet._json['entities']['hashtags']], tweet.user.followers_count])
-        consumer_key = raw_input('Consumer Key ')
-        consumer_secret = raw_input('Consumer Secret ')
-        access_token = raw_input('Access Token ')
-        access_token_secret = raw_input('Access Token Secret ')
-
-        hashtag_phrase = raw_input('Hashtag Phrase ')
-
-        if __name__ == '__main__':
-            search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase)
