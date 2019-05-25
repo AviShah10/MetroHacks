d60428ce0745a7632481cc4300f02086cf3e3d99
DanukGOAT <44783244+DanukGOAT@users.noreply.github.com>
2019-05-25 16:27:49 -0400

    Ayyy lets goooo

--- TestFile.py
+++ TestFile.py
@@ -3,15 +3,8 @@ import json
 import csv
 
 if __name__ == '__main__':
-    list_of_tweets = query_tweets("Trump OR Clinton", 10)
-
-    #print the retrieved tweets to the screen:
-    for tweet in query_tweets("Trump OR Clinton", 10):
-        print(tweet)
-
-    #Or save the retrieved tweets to file:
-    # file = open("output.txt","w")
     with open("alabama.csv", 'wt', encoding="utf8") as output:
         writer = csv.writer(output)
-        writer.writerows([tweet.text] for tweet in query_tweets("Abortion", 10))
-    # file.close()
+        writer.writerows([tweet.user] for tweet in query_tweets("Abortion", 10))
+
+# Or save the retrieved tweets to file:
